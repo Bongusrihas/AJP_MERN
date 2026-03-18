05684ed50818b349a025ba0c0ed4d59ccd5dd48b
Bongusrihas <164666377+Bongusrihas@users.noreply.github.com>
2026-03-18 13:49:38 +0530

    Delete exp_1 directory

--- exp_1/models/Faculty.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose"
-
-
-const facultyschema = new mongoose.Schema({
-    name: String,
-    subject:String
-});
-
-export default mongoose.model("faculty",facultyschema)
